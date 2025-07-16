6dc0255b6bfe70009a1e9a17d4a85218f043f005
Gavin Hellyer <gavin@gva-development.co.uk>
2025-07-16 11:32:33 +0100

    MainActivity - updated bridge to java code

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,32 @@
 package io.ionic.starter;
 
-import com.getcapacitor.BridgeActivity;
+import android.view.KeyEvent;
+import android.util.Log;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.ultimafurniture.lynx.LynxPlugin;
+
+public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Log.i(TAG, "dispatchKeyEvent: " + event);
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+        int keyCode = event.getKeyCode();
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_F4:
+                Log.i(TAG, "KeyEvent.KEYCODE_F4");
+                LynxPlugin plugin = (LynxPlugin) getBridge().getPlugin("LynxPlugin").getInstance();
+                plugin.startScan();
+                break;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+}
